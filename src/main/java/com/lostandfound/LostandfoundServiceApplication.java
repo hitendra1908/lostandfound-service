0e648893d83c5b55d32b0c649bd05207b0037b56
hitendra1908 <hitendra1908@gmail.com>
2024-08-09 20:15:01 +0200

    Spring Boot project lostandfound-service base structure

---
+++ src/main/java/com/lostandfound/LostandfoundServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lostandfound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostandfoundServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostandfoundServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lostandfound/LostandfoundServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lostandfound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostandfoundServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
